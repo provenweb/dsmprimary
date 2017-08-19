dbb4ebb845a8fcc4c82336619505545fa827b464
Proven Web <sales@provenweb.co.uk>
2017-08-19 13:02:26 +0100

    Revert "Change to DSM Primary"
    This reverts commit 19a44d4c45d4f179523c4f6c6c6d972edebeaf6c.

--- api/getcategories.php
+++ api/getcategories.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 include '../../wp-config.php';
 
-$sql = "SELECT name,slug FROM wp_term_taxonomy INNER JOIN wp_terms ON (wp_terms.term_id = wp_term_taxonomy.term_id) WHERE wp_term_taxonomy.taxonomy = 'category' AND slug!='events' AND slug!='misc' AND slug!='newsletter' AND slug!='clubs' ORDER by name" ;
+$sql = "SELECT name,slug FROM wp_7b6fbac120_term_taxonomy INNER JOIN wp_7b6fbac120_terms ON (wp_7b6fbac120_terms.term_id = wp_7b6fbac120_term_taxonomy.term_id) WHERE wp_7b6fbac120_term_taxonomy.taxonomy = 'category' AND slug!='events' AND slug!='misc' AND slug!='newsletter' AND slug!='clubs' ORDER by name" ;
 
 try {
 	$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
@@ -10,7 +10,21 @@ try {
 	$stmt = $dbh->query($sql);
 	$categories = $stmt->fetchAll(PDO::FETCH_OBJ);
 	$dbh = null;
-	echo '{"items":'. json_encode($categories) .'}';
+
+	$catstr=json_encode($categories);
+	$lookfor = '{"name":"7 Blue","slug":"7blue"},{"name":"7 Red","slug":"7red"},';
+	if (strpos($catstr,$lookfor)>0) {
+		$catstr=str_replace($lookfor,"",$catstr);
+	}
+	$lookfor = substr($lookfor,0,-1);
+	$catstr=str_replace('"}]','"},'.$lookfor."]",$catstr);
+	$lookfor = ',{"name":"STEM","slug":"stem"}';
+	if (strpos($catstr,$lookfor)>0) {
+		$catstr=str_replace($lookfor,"",$catstr);
+	}
+	//$catstr=str_replace('"}]','"}'.$lookfor."]",$catstr);
+	echo '{"items":'. $catstr .'}';
+
 } catch(PDOException $e) {
 	echo '{"error":{"text":'. $e->getMessage() .'}}';
 }
