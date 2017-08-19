aadc4de36c2cd5a8529647ecde1f25a860e03040
Proven Web <sales@provenweb.co.uk>
2017-08-19 13:09:47 +0100

    Change to DSM Primary attempt 2

--- api/getinfolist.php
+++ api/getinfolist.php
@@ -2,7 +2,9 @@
 header("Access-Control-Allow-Origin: *");
 include '../../wp-config.php';
 
-$sql = "SELECT p2.ID,p2.post_title,p2.post_name FROM wp_7b6fbac120_term_relationships tr INNER JOIN wp_7b6fbac120_posts p ON tr.object_id = p.id INNER JOIN wp_7b6fbac120_postmeta pm ON pm.post_id = p.id INNER JOIN wp_7b6fbac120_posts p2 ON pm.meta_value = p2.id WHERE tr.term_taxonomy_id='164' ORDER BY p.menu_order" ;
+$sql = "SELECT p2.ID,p2.post_title,p2.post_name FROM wp_term_relationships tr JOIN wp_posts p ON tr.object_id=p.id JOIN wp_postmeta pm on pm.post_id = p.id JOIN wp_posts p2 on p2.id = pm.meta_value WHERE tr.term_taxonomy_id = 170 ORDER BY p.menu_order;" ;
+
+$sql = "SELECT ID,post_title,post_name FROM wp_posts WHERE post_type='page'";
 
 try {
 	$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
--- api/getinfopages.php
+++ api/getinfopages.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 include '../../wp-config.php';
 
-$sql = "SELECT name,slug FROM wp_7b6fbac120_term_taxonomy INNER JOIN wp_7b6fbac120_terms ON (wp_7b6fbac120_terms.term_id = wp_7b6fbac120_term_taxonomy.term_id) WHERE wp_7b6fbac120_term_taxonomy.taxonomy = 'category' AND slug!='events' AND slug!='misc' AND slug!='newsletter' AND slug!='clubs' ORDER by name" ;
+$sql = "SELECT name,slug FROM wp_term_taxonomy INNER JOIN wp_terms ON (wp_terms.term_id = wp_term_taxonomy.term_id) WHERE wp_term_taxonomy.taxonomy = 'category' AND slug!='events' AND slug!='misc' AND slug!='newsletter' AND slug!='clubs' ORDER by name" ;
 
 try {
 	$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
